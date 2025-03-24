9282b391c6d1e7f9b3302411173f7afe192dfab3
prayanne <kyhy0321@gmail.com>
2025-03-24 19:54:22 +0800

    first

---
+++ app/src/main/java/com/example/journey/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.journey
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.journey.databinding.ActivityMainBinding
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        enableEdgeToEdge()
+        ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val intent = Intent(this, ActivityLogin::class.java)
+        binding.go.setOnClickListener { startActivity(intent) }
+    }
+}
